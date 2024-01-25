cb58292e65a1c5bc958c913f8a9dc24b3d491d47
loganrun <bigdog_41@hotmai.com>
2024-01-24 23:33:07 -0800

    another refactor

--- index.js
+++ index.js
@@ -77,6 +77,89 @@ const CourseInfo = {
     }
   ];
 
+  // const courseValidate=(assignment, courseInfo) =>{
+  //   let courseId= courseInfo.id
+  //   let assignCheck = assignment.course_id
+
+  //   if(courseId === assignCheck){
+  //       return true
+  //   }else{
+  //       return false
+  //   }
+  // }
+
+  // const AssignmentChecker = (assignment, workSubmitted) =>{
+  //   const dateCheck = assignment.assignments
+  //   const submissions = workSubmitted
+  //   let currentCourse = new Set()
+
+  //   try {
+  //       for(date of dateCheck){
+  //         let inputDate = date.due_at
+  //         let currentDate = new Date()
+  //         let currentDateString = currentDate.toISOString().slice(0,10)
+  //         //console.log(inputDate)
+  //         //console.log(currentDateString)
+  //         if(inputDate <= currentDateString){
+  //           currentCourse.add(date.id)
+  //           validAssign.push(date)
+          
+  //         }else{
+  //           console.log(`The assignment "${date.name}" is not due.`)
+  //         }
+  //       } 
+  //       //console.log(validAssign)
+  //     }catch (error) {
+  //       console.error(error)
+  //   } finally{
+  //     for(assign of submissions){
+  //       //console.log(submissions)
+  //       if(currentCourse.has(assign.assignment_id)){
+  //         validSubmissions.push(assign)
+  //       }else{
+  //         console.log(`Assignment ${assign.assignment_id} cannot be graded.`)
+  //       }
+  //     }
+  //     for(assgn of validAssign){
+  //       console.log(assgn.id)
+  //     }
+
+  //      console.log(validAssign)
+  //     // console.log(validSubmissions)
+
+  //   //   finalGrade(validAssign,validSubmissions)
+  //   }
+  //   // console.log(validAssign)
+  //   // console.log(validSubmissions)
+
+  //   return 
+  // }
+
+  // const finalGrade= async(validAssign, validSubmissions)=>{
+
+  //   const learner = await validSubmissions
+
+  //   console.log(learner)
+  //   console.log(validSubmissions)
+
+  // }
+
+  
+
+
+const getLearnerData = async (course, ag, submissions)=> {
+  let info = course;
+  let assignment = ag;
+  let workSubmitted = submissions
+
+  // const finalGrade= async(validAssign, validSubmissions)=>{
+
+  //   const learner = await validSubmissions
+
+  //   console.log(learner)
+  //   console.log(validSubmissions)
+
+  // }
   const courseValidate=(assignment, courseInfo) =>{
     let courseId= courseInfo.id
     let assignCheck = assignment.course_id
@@ -120,57 +203,69 @@ const CourseInfo = {
           console.log(`Assignment ${assign.assignment_id} cannot be graded.`)
         }
       }
+      for(assgn of validAssign){
+         let finalCheck = assgn
+      }
+      for(sub of validSubmissions){
+        let student = sub
+      }
+
+      
+
+       console.log(validAssign)
+      console.log(validSubmissions)
 
     //   finalGrade(validAssign,validSubmissions)
     }
-    console.log(validAssign)
-    console.log(validSubmissions)
+    // console.log(validAssign)
+    // console.log(validSubmissions)
 
-    //return validSubmissions
+    return 
   }
 
-  const finalGrade=()=>{
-
-  }
-
-
-const getLearnerData = async (course, ag, submissions)=> {
-  let info = course;
-  let assignment = ag;
-  let workSubmitted = submissions
-  
-
-    
+    try{
       if(courseValidate(assignment,info) == true){
         AssignmentChecker(assignment, workSubmitted)
     } else {
         throw new Error("These are not the right assignments for this course")
     }
+
+    }catch(error){
+      
+      console.error(error)
+    }
+    finally{
+
+      // finalGrade()
+
+    }
+     
     
 
-    const finalGrade=()=>{
-      
-    }
+    
+
+    // console.log(validAssign)
+      // console.log(validSubmissions)
     // here, we would process this data to achieve the desired result.
-     const result = finalGrade()
-    //[
-    //   {
-    //     id: 125,
-    //     avg: 0.985, // (47 + 150) / (50 + 150)
-    //     1: 0.94, // 47 / 50
-    //     2: 1.0 // 150 / 150
-    //   },
-    //   {
-    //     id: 132,
-    //     avg: 0.82, // (39 + 125) / (50 + 150)
-    //     1: 0.78, // 39 / 50
-    //     2: 0.833 // late: (140 - 15) / 150
-    //   }
-    // ];
+     const result = 
+    [
+      {
+        id: 125,
+        avg: 0.985, // (47 + 150) / (50 + 150)
+        1: 0.94, // 47 / 50
+        2: 1.0 // 150 / 150
+      },
+      {
+        id: 132,
+        avg: 0.82, // (39 + 125) / (50 + 150)
+        1: 0.78, // 39 / 50
+        2: 0.833 // late: (140 - 15) / 150
+      }
+    ];
   
     return result;
   }
   
   const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
   
-  console.log(result);
+  //console.log(result);
